a7d337f626ea80185bad06362c6f281e4dd26f3f
santilafu <santi10dy@gmail.com>
2025-05-22 11:41:17 +0200

    Entidad Cliente corregida

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,42 +1,42 @@
-import java.util.*;
 import javax.persistence.*;
 import java.io.Serializable;
 
 /**
- * Clase que representa un cliente.
- * Que será guardado en la base de datos.
+ * Clase que representa a un cliente que será guardado en la base de datos.
  */
-@Entity //Esta clase se va a almacenar como entidad en ObjectDB
-public class Cliente implements Serializable{
+@Entity // Esta clase se va a almacenar como entidad en ObjectDB
+public class Cliente implements Serializable {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private long id;
+
     private String nombre;
 
+    // Constructor vacío obligatorio para ObjectDB
     public Cliente() {}
 
+    // Constructor para facilitar la creación de objetos
     public Cliente(String nombre) {
         this.nombre = nombre;
     }
 
-    public Long getId() {
+    // Getters y setters necesarios para que ObjectDB pueda acceder a los campos
+    public long getId() {
         return id;
     }
-    public void setId(Long id) {
-        this.id = id;
-    }
+
     public String getNombre() {
         return nombre;
     }
+
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
+    // Método que muestra los datos del cliente
     @Override
     public String toString() {
-        return "Cliente{" +
-                "id=" + id +
-                ", nombre='" + nombre + '\'' +
-                '}';
+        return "Cliente{id=" + id + ", nombre='" + nombre + "'}";
     }
 }
